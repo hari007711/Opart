65c43087c2e0a0ac44149dffc6da5ae4525c03b4
hari007711 <hari0077kumar@gmail.com>
2025-10-06 11:26:59 +0530

    url 7

--- src/app/api/auth/[...betterauth]/route.ts
+++ src/app/api/auth/[...betterauth]/route.ts
@@ -1,5 +1,5 @@
 // app/api/auth/[...betterauth]/route.ts
 import { auth } from "@/lib/auth";
-import { betterAuthHandler } from "better-auth/next-js";
+import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = betterAuthHandler(auth);
+export const { GET, POST } = toNextJsHandler(auth);
